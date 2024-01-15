b97f40258a045567d23cbb1f67f9e661aa032e23
Ayush kumar <kumarayushkumar@protonmail.com>
2024-01-15 17:52:47 +0530

    Add compression and helmet middleware

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,14 @@ dotenv.config()
 import http from 'http'
 import express from 'express'
 import { ROUTER } from './router'
+import helmet from 'helmet'
+import compression from 'compression'
 
 const app = express()
 
 const server = () => {
+  app.use(helmet())
+  app.use(compression())
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
 
