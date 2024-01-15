87cc04dbc5f4e596f602e7e0e5fc27524024ecad
Ayush kumar <kumarayushkumar@protonmail.com>
2024-01-15 18:06:30 +0530

    Fix import statements in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
+import sentry from '@sentry/astro'
+import spotlightjs from '@spotlightjs/astro'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sentry(), spotlightjs()]
-});
+})
